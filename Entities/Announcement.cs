eb75d0a4bd547fc3cde0b0a8f088f52661bea3f0
Serkan Kumru <serkankumru1997@gmail.com>
2023-09-14 14:30:15 +0300

    Add files via upload

---
+++ Entities/Announcement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Announcement:EntityBase
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
